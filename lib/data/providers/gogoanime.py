243f3467dae4ac1a8d520813c027542147d61a4a
Kayvon-Martinez <142514475+Kayvon-Martinez@users.noreply.github.com>
2023-09-21 04:03:49 -0500

    Started Gogoanime Provider.
    Set up Gogoanime Provider so that it will check for the latest base and ajax url for Gogoanime when an instance of the class is created.

--- lib/data/providers/gogoanime.py
+++ lib/data/providers/gogoanime.py
@@ -1,4 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
 
 
 class GogoanimeProvider:
-    pass
+    base_url = "https://gogoanime3.net"
+    ajax_url = "https://ajax.gogo-load.com/ajax"
+
+    def __init__(self) -> None:
+        GogoanimeProvider.base_url = GogoanimeProvider.check_url_for_redirect(
+            GogoanimeProvider.base_url)
+        GogoanimeProvider.ajax_url = GogoanimeProvider.check_url_for_redirect(
+            GogoanimeProvider.ajax_url)
+
+    @staticmethod
+    def check_url_for_redirect(url: str, **kwargs) -> str:
+        response = requests.get(url)
+        return response.url
